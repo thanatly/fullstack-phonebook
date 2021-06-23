3fd678ed83707b27fc77a83537bbff133cbab19e
thanatly <82842370+thanatly@users.noreply.github.com>
2021-06-23 22:46:44 +0300

    Commit for ex. 3.10

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const morgan = require('morgan')
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 const cors = require('cors')
-
 app.use(cors())
+app.use(express.static('build'))
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })

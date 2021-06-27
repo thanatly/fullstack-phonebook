42b7f26a668bc7ae273899656cabe1e2673035a8
thanatly <82842370+thanatly@users.noreply.github.com>
2021-06-27 23:07:25 +0300

    Commit for ex 3.14

--- index.js
+++ index.js
@@ -20,29 +20,6 @@ morgan.token('data', function (req, res) {
   return JSON.stringify(req.body) 
 })
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>')
 })
@@ -59,31 +36,18 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-  response.json(person)
+  Contact.findById(request.params.id).then(contact =>
+    response.json(contact)
+  )
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Contact.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
 })
-
-const generateId = () => {
-  const randId = Math.floor(Math.random()*1000)
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + randId + 1
-}
-
+s
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -93,28 +57,21 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const sameName = persons.find(person => person.name === body.name)
-  if (sameName) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
-
   if (!body.number) {
     return response.status(400).json({ 
       error: 'number missing' 
     })
   }
 
-  const person = {
+  // Need to prevent name duplication
+  const contact = new Contact({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  contact.save().then(savedContact => {
+    response.json(savedContact)
+  })
 
 })
 

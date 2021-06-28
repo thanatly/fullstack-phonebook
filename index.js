090954f21c432409d146ff653b2d39f7f41a3b87
thanatly <82842370+thanatly@users.noreply.github.com>
2021-06-28 13:29:15 +0300

    Commit for ex 3.19

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.delete('/api/persons/:id', (request, response) => {
   .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -84,7 +84,6 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
   .catch(error => next(error))
-
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -113,7 +112,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
